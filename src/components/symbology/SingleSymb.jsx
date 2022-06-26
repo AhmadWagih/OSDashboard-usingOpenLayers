8bfd26e46dbf4be743404566275818c8bab4762f
Ahmad Wagih <AhmWagih@gmail.com>
2022-06-26 23:31:21 +0200

    connected to backend

---
+++ src/components/symbology/SingleSymb.jsx
@@ -0,0 +1,27 @@
+
+const SingleSymb = () => {
+    return ( 
+        <>
+ <div className="list-group-item">
+        <h4>Single Symbology</h4>
+      </div>
+      <div className="component-div border-bot">
+        <label htmlFor="layer-name" className="label-dark">
+          Layer Name
+        </label>
+        <input
+          onChange={handleChange}
+          value={state.layerName}
+          name="layerName"
+          className="text-input-dark"
+          placeholder="Layer1"
+          id="layer-name"
+        />
+      </div>
+ 
+        
+        </>
+     );
+}
+ 
+export default SingleSymb;
