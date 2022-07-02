87fb541bdf6c7f19f7dda5013350597c2ff3c865
HasanAlAswad <97350417+Hasan-AlAswad@users.noreply.github.com>
2022-07-02 04:45:11 +0200

    loginCSS-Done

--- src/components/Common/LoginO.jsx
+++ src/components/Common/LoginO.jsx
@@ -5,8 +5,10 @@ import { LoginUser } from "../../APIs/auth";
 // import AuthContext from '../context/AuthProvider';
 // // import axios from 'axios';
 // // const LOGIN_URL ='/auth';
+import { ToastContainer } from "react-toastify";
+
+import "../../styles/Login.css";
 import classes from "../../styles/index.module.css";
-import { ToastContainer } from 'react-toastify';
 
 const LoginO = () => {
   // const {setAuth} = useContext(AuthContext)
@@ -41,8 +43,8 @@ const LoginO = () => {
 
   return (
     <>
-    <ToastContainer/>
-      <section>
+      <ToastContainer />
+      <section style={{margin:"auto"}}>
         <p
           ref={errRef}
           className={errMsg ? "errmsg" : "offscreen"}
@@ -50,38 +52,101 @@ const LoginO = () => {
         >
           {errMsg}
         </p>
-        <h1 className={classes.headerform}>Sign In</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            ref={userRef}
-            autoComplete="off"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-            required
-          />
-          <br />
-          <button>Sign In</button>
-        </form>
-        <p>
-          Need an Account?
-          <br />
-          <span className={classes.line}>
-            {/*put router link here*/}
-            <Link to="/register">Sign Up</Link>
-          </span>
-        </p>
+        <div className={classes.container}>
+          <div className="row">
+            <div className="col-md-5 mx-auto">
+              <div id="first">
+                <div className="formstyle  ">
+                  <div className="logo mb-3">
+                    <div className="col-md-12 text-center">
+                      <h1 className={classes.headerform}>Login</h1>
+                    </div>
+                  </div>
+
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="username" className="Uform">
+                        Username:
+                      </label>
+                      <input
+                        className="form-control general"
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}
+                        required
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="password" className="Uform general">
+                        Password:
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        className="form-control"
+                        required
+                      />
+                    </div>
+
+                    {/* <div className="form-group">
+                      <p className="text-center Uform">
+                        By signing up you accept our
+                        <Link to="/register" id="term">
+                          Terms Of Use
+                        </Link>
+                      </p>
+                    </div> */}
+
+                    <div className="col-md-12 text-center ">
+                      <button
+                        type="submit"
+                        // className=" btn btn-block btnstyle btn-primary tx-tfm general m-2"
+                        className={classes.btn}
+                      >
+                        Login
+                      </button>
+                    </div>
+
+                    <div className="col-md-12 ">
+                      <div className="orstyle">
+                        <hr className="orline" />
+                        <span className="span-or">or</span>
+                      </div>
+                    </div>
+                    <div className="col-md-12 mb-3">
+                      <p className="text-center">
+                        <Link to="/register" className="google btn btnstyle">
+                          <i className="fa-brands fa-google-plus-g"></i> Signup
+                          using Google
+                        </Link>
+                      </p>
+                    </div>
+
+                    <div className="form-group">
+                      <p className="Uform text-center">
+                        Don't have account?
+                        <br />
+                        <span className={classes.line}>
+                          {/*put router link here*/}
+                          <Link className={classes.btn} to="/register" id="signup">
+                            Sign up here
+                          </Link>
+                        </span>
+                      </p>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/components/Common/Register.jsx
+++ src/components/Common/Register.jsx
@@ -2,178 +2,290 @@ import { useRef, useState, useEffect } from "react";
 // import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import classes from "../../styles/index.module.css"
-import {useNavigate} from "react-router-dom"
-import { RegisterUser } from './../../APIs/auth';
-import { ToastContainer } from 'react-toastify';
+import classes from "../../styles/index.module.css";
+import { useNavigate } from "react-router-dom";
+import { RegisterUser } from "./../../APIs/auth";
+import { ToastContainer } from "react-toastify";
+
+import "../../styles/Login.css";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-// const REGISTER_URL = '/register';
+const REGISTER_URL = "/register";
 
 const Register = () => {
-    const userRef = useRef();
-    const errRef = useRef();
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [user, setUser] = useState('');
-    const [validName, setValidName] = useState(false);
-    const [userFocus, setUserFocus] = useState(false);
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [email, setEmail] = useState('');
-    
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
+  const [email, setEmail] = useState("");
 
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [errMsg, setErrMsg] = useState('');
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
-    const navigate = useNavigate();
+  const [errMsg, setErrMsg] = useState("");
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setValidName(USER_REGEX.test(user));
-    }, [user])
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        setValidPwd(PWD_REGEX.test(pwd));
-        setValidMatch(pwd === matchPwd);
-    }, [pwd, matchPwd])
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [user]);
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd, matchPwd])
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        // if button enabled with JS hack
-        const v1 = USER_REGEX.test(user);
-        const v2 = PWD_REGEX.test(pwd);
-        if (!v1 || !v2) {
-            setErrMsg("Invalid Entry");
-            return;
-        }
-        const response = await RegisterUser(user,email,pwd);
-        //clear state and controlled inputs
-        //need value attrib on inputs for this
-        console.log(response);
-        if (response) {
-            setUser('');
-            setPwd('');
-            setMatchPwd('');
-            navigate("/login");
-        }
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // if button enabled with JS hack
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return;
     }
+    const response = await RegisterUser(user, email, pwd);
+    //clear state and controlled inputs
+    //need value attrib on inputs for this
+    console.log(response);
+    if (response) {
+      setUser("");
+      setPwd("");
+      setMatchPwd("");
+      navigate("/login");
+    }
+  };
 
-    return (
-        <>
-        <ToastContainer/>
-                <section>
-                    <p ref={errRef} className={errMsg ? classes.errmsg : classes.offscreen} aria-live="assertive">{errMsg}</p>
-                    <h1 className={classes.headerform}>Register</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">
-                            Username:
-                            <i className={validName ? "fa-solid fa-check "+classes.valid : "fa-solid fa-check d-none"} ></i>
-                            <i className={validName || !user ? "fa-solid fa-times d-none" : "fa-solid fa-times "+classes.invalid} ></i>
-                        </label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                            aria-invalid={validName ? "false" : "true"}
-                            aria-describedby="uidnote"
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                        />
-                        <p id="uidnote" className={userFocus && user && !validName ? classes.instructions : classes.offscreen}>
-                            {/* <FontAwesomeIcon icon={faInfoCircle} /> */}
-                            <i className="fa-solid fa-info-circle" ></i>
-                            4 to 24 characters.<br />
-                            Must begin with a letter.<br />
-                            Letters, numbers, underscores, hyphens allowed.
-                        </p>
-                        <label htmlFor="email">
-                            Email:
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                            required
-                        />
-
-                        <label htmlFor="password">
-                            Password:
-                            <i className={validPwd ? "fa-solid fa-check "+classes.valid : "fa-solid fa-check d-none"} ></i>
-                            <i className={validPwd || !pwd ? "fa-solid fa-times d-none" : "fa-solid fa-times "+classes.invalid} ></i>
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdnote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-                        />
-                        <p id="pwdnote" className={pwdFocus && !validPwd ? classes.instructions : classes.offscreen}>
-                            <i className="fa-solid fa-info-circle" ></i>
-                            8 to 24 characters.<br />
-                            Must include uppercase and lowercase letters, a number and a special character.<br />
-                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-                        </p>
-
-
-                        <label htmlFor="confirm_pwd">
-                            Confirm Password:
-                            <i className={validMatch && matchPwd ? "fa-solid fa-check "+classes.valid : "fa-solid fa-check d-none"} ></i>
-                            <i className={validMatch || !matchPwd ? "fa-solid fa-times d-none" : "fa-solid fa-times "+classes.invalid} ></i>
-                            {/* <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
-                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} /> */}
-                        </label>
-                        <input
-                            type="password"
-                            id="confirm_pwd"
-                            onChange={(e) => setMatchPwd(e.target.value)}
-                            value={matchPwd}
-                            required
-                            aria-invalid={validMatch ? "false" : "true"}
-                            aria-describedby="confirmnote"
-                            onFocus={() => setMatchFocus(true)}
-                            onBlur={() => setMatchFocus(false)}
-                        />
-                        <p id="confirmnote" className={matchFocus && !validMatch ? classes.instructions : classes.offscreen}>
-                            {/* <FontAwesomeIcon icon={faInfoCircle} /> */}
-                            <i className="fa-solid fa-info-circle" ></i>
-                            Must match the first password input field.
-                        </p>
-
-                        <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
-                    </form>
-                    <p>
-                        Already registered?<br />
-                        <span className={classes.line}>
-                        <Link to="/Login">Sign In</Link>
-                        </span>
+  return (
+    <>
+      <ToastContainer />
+      <section className="Appbody">
+        <p
+          ref={errRef}
+          className={errMsg ? classes.errmsg : classes.offscreen}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <div className={classes.container}>
+          <div className="row">
+            <div className="col-md-5 mx-auto">
+              <div id="seconed">
+                <div className="formstyle  ">
+                  <div className="logo mb-3">
+                    <div className="col-md-12 text-center">
+                      <h1 className={classes.headerform}>Signup</h1>
+                    </div>
+                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="username" className="Uform">
+                        Username:
+                        <i
+                          className={
+                            validName
+                              ? "fa-solid fa-check " + classes.valid
+                              : "fa-solid fa-check d-none"
+                          }
+                        ></i>
+                        <i
+                          className={
+                            validName || !user
+                              ? "fa-solid fa-times d-none"
+                              : "fa-solid fa-times " + classes.invalid
+                          }
+                        ></i>
+                      </label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}
+                        required
+                        aria-invalid={validName ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setUserFocus(true)}
+                        onBlur={() => setUserFocus(false)}
+                      />
+                    </div>
+                    <p
+                      id="uidnote"
+                      className={
+                        userFocus && user && !validName
+                          ? classes.instructions
+                          : classes.offscreen
+                      }
+                    >
+                      {/* <FontAwesomeIcon icon={faInfoCircle} /> */}
+                      <i className="fa-solid fa-info-circle"></i>
+                      4 to 24 characters.
+                      <br />
+                      Must begin with a letter.
+                      <br />
+                      Letters, numbers, underscores, hyphens allowed.
                     </p>
-                </section>
-            
-        </>
-    )
-}
+                    <div className="form-group">
+                      <label htmlFor="email" className="Uform">
+                        Email:
+                      </label>
+                      <input
+                        className="form-control"
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                      />
+                    </div>
 
-export default Register
+                    <div className="form-group">
+                      <label htmlFor="password" className="Uform">
+                        Password:
+                        <i
+                          className={
+                            validPwd
+                              ? "fa-solid fa-check " + classes.valid
+                              : "fa-solid fa-check d-none"
+                          }
+                        ></i>
+                        <i
+                          className={
+                            validPwd || !pwd
+                              ? "fa-solid fa-times d-none"
+                              : "fa-solid fa-times " + classes.invalid
+                          }
+                        ></i>
+                      </label>
+                      <input
+                        className="form-control"
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        required
+                        aria-invalid={validPwd ? "false" : "true"}
+                        aria-describedby="pwdnote"
+                        onFocus={() => setPwdFocus(true)}
+                        onBlur={() => setPwdFocus(false)}
+                      />
+                    </div>
+                    <p
+                      id="pwdnote"
+                      className={
+                        pwdFocus && !validPwd
+                          ? classes.instructions
+                          : classes.offscreen
+                      }
+                    >
+                      <i className="fa-solid fa-info-circle"></i>
+                      8 to 24 characters.
+                      <br />
+                      Must include uppercase and lowercase letters, a number and
+                      a special character.
+                      <br />
+                      Allowed special characters:{" "}
+                      <span aria-label="exclamation mark">!</span>{" "}
+                      <span aria-label="at symbol">@</span>{" "}
+                      <span aria-label="hashtag">#</span>{" "}
+                      <span aria-label="dollar sign">$</span>{" "}
+                      <span aria-label="percent">%</span>
+                    </p>
+
+                    <div className="form-group">
+                      <label htmlFor="confirm_pwd" className="Uform">
+                        Confirm Password:
+                        <i
+                          className={
+                            validMatch && matchPwd
+                              ? "fa-solid fa-check " + classes.valid
+                              : "fa-solid fa-check d-none"
+                          }
+                        ></i>
+                        <i
+                          className={
+                            validMatch || !matchPwd
+                              ? "fa-solid fa-times d-none"
+                              : "fa-solid fa-times " + classes.invalid
+                          }
+                        ></i>
+                        {/* <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} /> */}
+                      </label>
+                      <input
+                        className="form-control"
+                        type="password"
+                        id="confirm_pwd"
+                        onChange={(e) => setMatchPwd(e.target.value)}
+                        value={matchPwd}
+                        required
+                        aria-invalid={validMatch ? "false" : "true"}
+                        aria-describedby="confirmnote"
+                        onFocus={() => setMatchFocus(true)}
+                        onBlur={() => setMatchFocus(false)}
+                      />
+                    </div>
+                    <p
+                      id="confirmnote"
+                      className={
+                        matchFocus && !validMatch
+                          ? classes.instructions
+                          : classes.offscreen
+                      }
+                    >
+                      {/* <FontAwesomeIcon icon={faInfoCircle} /> */}
+                      <i className="fa-solid fa-info-circle"></i>
+                      Must match the first password input field.
+                    </p>
+
+                    <div className="col-md-12 text-center mb-3">
+                      <button
+                        type="submit"
+                        className=" btn btn-block btnstyle btn-primary tx-tfm m-3"
+                        disabled={
+                          !validName || !validPwd || !validMatch ? true : false
+                        }
+                      >
+                        Get Started For Free
+                      </button>
+                    </div>
+                    <div className="col-md-12 ">
+                      <div className="form-group">
+                        <p className="text-center">
+                          <Link to="/login" id="signin">
+                            Already have an account?
+                          </Link>
+                        </p>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;
